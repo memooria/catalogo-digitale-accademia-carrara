ac61f805874b9ae2db65ec4df75db826fc9d07cd
work@communicationbox.it <StratoCe83_7@>
2024-09-04 10:34:29 +0200

    Fix titleCase.js

--- _plugins/filters/titleCase.js
+++ _plugins/filters/titleCase.js
@@ -5,8 +5,13 @@
  * @return     {String}  The input string converted to title case
  */
 module.exports = function(string) {
-  return string
+  /*
+    return string
     .split(/(?=[A-Z])|[_\s]/)
     .map((word) => word.replace(word[0], word[0].toUpperCase()))
     .join(' ')
+   */
+
+  // Fix per evitare che ogni iniziale di parola sia maiuscola.
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
 }
