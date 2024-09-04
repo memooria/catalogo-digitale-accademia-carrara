4f4294bf30ecc48cc20123c815e388f92f1830d1
work@communicationbox.it <StratoCe83_7@>
2024-09-04 10:27:25 +0200

    Ripristinata capitalize.js

--- _plugins/filters/capitalize.js
+++ _plugins/filters/capitalize.js
@@ -1,5 +1,3 @@
-module.exports = function(string) {
-  // return string.charAt(0).toUpperCase() + string.slice(1)
-
-  return string
+module.exports = function (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
